58a676efae8e8082b53440edefac2b5a3f9445b4
zhangcheng <zh.milo@gmail.com>
2022-11-30 12:11:19 +0800

    update ignore

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,24 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const ethers = require('ethers');
+require('dotenv').config()
+const encryptWalletJson = require('./master.json');
+const wallet = ethers.Wallet.fromEncryptedJsonSync(JSON.stringify(encryptWalletJson), process.env.COMMON_KEY)
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: process.env.RPC_EVM,
+      accounts: [wallet.privateKey]
+    }
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1000,
+    },
+  },
 };
